9a6de9fbcf5a0be313c5b345daf7157388756baa
VuongNguyen <enlove2005@gmail.com>
2024-08-14 08:01:22 +0700

    update 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import B1 from '@/components/B1'
+import B10 from '@/components/B10/B10'
 import B2 from '@/components/B2'
 import B3 from '@/components/B3'
+import B9 from '@/components/B9/B9'
+import B6 from '@/components/Bt6'
+import B4 from 'Bt4'
+import B5 from '@/components/Bt5'
+import B8 from '@/components/Bt8'
 import React from 'react'
 
 export default function page() {
@@ -9,6 +15,12 @@ export default function page() {
       <B1/>
       <B2/>
       <B3/>
+      <B4/>
+      <B5/>
+      <B6/>
+      <B8/>
+      <B9/>
+      <B10/>
     </div>
   )
 }
